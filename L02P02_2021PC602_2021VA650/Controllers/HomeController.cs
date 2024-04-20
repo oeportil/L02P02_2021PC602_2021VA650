97ac044b0005875d7a3f964a1b6f3908086204d4
Oscar Portillo <Oscaritoportilllo@outlook.com>
2024-04-20 14:12:59 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021PC602_2021VA650/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using L02P02_2021PC602_2021VA650.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace L02P02_2021PC602_2021VA650.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
